685c8a124f058ff712e8f373efd2374483c8c381
Mateus Herculano <mateuherculano0@gmail.com>
2022-07-18 13:54:45 -0300

    feat: components Input/Button

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,12 +1,46 @@
-import { VStack, Heading } from 'native-base';
+import { useState } from 'react';
+import { VStack, Heading, Icon, useTheme } from 'native-base';
+import { Envelope, Key } from 'phosphor-react-native';
+
+import Logo from '../assets/logo_primary.svg';
+
+import { Input } from '../components/Input';
+import { Button } from '../components/Button';
 
 export function SignIn() {
+  const { colors } = useTheme();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <VStack flex={1} alignItems='center' bg="gray.600" px={8} pt={24}>
+      <Logo />
 
       <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
         Acesse sua conta
       </Heading>
+
+      <Input
+        value={email}
+        onChangeText={setEmail}
+        mb={4}
+        placeholder="E-mail"
+        InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} />} ml={4} />}
+      />
+      <Input
+        value={password}
+        onChangeText={setPassword}
+        mb={8}
+        placeholder="Senha"
+        InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
+        secureTextEntry
+      />
+
+      <Button
+        title="Entrar"
+        w="full"
+      />
 
     </VStack>
   )
