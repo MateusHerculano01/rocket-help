464c213790ebfd03939a9fae955fa4945efe359c
Mateus Herculano <mateuherculano0@gmail.com>
2022-07-18 12:21:33 -0300

    feat: project init

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+
+import { SignIn } from './src/screens/SignIn';
+
+export default function App() {
+  return (
+    <SignIn />
+  );
+}
+
